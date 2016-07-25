f33e225f965c407530b2795fd43aeae27f78c599
Miao Yan <miaoyanapp@gmai.com>
2016-07-24 20:02:53 -0700

    linkedList second commit

--- linkedList/temp.cpp
+++ linkedList/temp.cpp
@@ -346,5 +346,211 @@ void unorderedLinkedList<Type>::insertLast(const Type& newItem)
 template <class Type>
 void unorderedLinkedList<Type>::deleteNode(const Type& deleteItem)
 {
-	
+	nodeType<Type> *current;
+	nodeType<Type> *priorCur;
+	bool found;
+
+	if(first == NULL)
+		cout << "Cannot delete from an empty list" << endl;
+	else{
+		if (first->info == deleteItem)
+		{
+			current = first;
+			first = first->link;
+			count--;
+			if (first == NULL)
+				last = NULL;
+			delete current;
+		}
+		else
+		{
+			found = false;
+			priorCur = first;
+			current = first->link;
+
+			while (current != NULL && !found)
+			{
+				if (current->info != deleteItem)
+				{
+					priorCur = current;
+					current = current->link;
+				}
+				else
+				{
+					found = true;
+				}
+			}
+
+			if (found)
+			{
+				priorCur->link = current->link;
+				count--;
+				if (last == current)
+					last = priorCur;
+				delete current
+			}
+			else{
+				cout << "The item to be delete is not in the list" << endl;
+			}
+		}
+	}
+}
+
+//************************************************************************************************************************************
+// Ordered Linked List assuming  order is ascending
+
+template <class Type>
+class orderedLinkedList: public linkedListType<Type>
+{
+public:
+	bool search(const Type& searchItem) const;
+
+	void insert(const Type& newItem);
+
+	void insertFirst(const Type& newItem);
+
+	void insertLast(const Type& newItem);
+
+	void deleteNode(const Type& deleteItem);
+
+};
+
+template <class Type>
+bool orderedLinkedList<Type>::search(const Type& searchItem) const
+{
+	bool found = fale;
+	nodeType<Type> *current;
+
+	current = first;
+
+	while(current != NULL && !found)
+	{
+		if(current->info >= searchItem)
+			found = true;
+		else
+			current = current->link;
+	}
+
+	if (found)
+		found = (current->info == searchItem);
+
+	return found;
+}
+
+template <class Type>
+void orderedLinkedList<Type>::insert(const Type& newItem)
+{
+	nodeType<Type> *current;
+	nodeType<Type> *trailCurrent;
+	nodeType<Type> *newNode;
+
+	bool found;
+
+	newNode = new nodeType<Type>;
+	newNode->info = newItem;
+	newNode->link = NULL;
+
+	if (first == NULL)
+	{
+		first = newNode;
+		last = newNode;
+		count++;
+	}
+	else
+	{
+		current = first;
+		found = false;
+		while (current != NULL && !found)
+			if (current->info >= newItem)
+				found = true;
+			else
+			{
+				trailCurrent = current;
+				current = current->link;
+			}
+
+		if (current == first)
+		{
+			newNode->link = first;
+			first = newNode;
+			count++;
+		}
+		else
+		{
+			trailCurrent->link = newNode;
+			newNode->link = current;
+
+			if (current = NULL)
+				last = newNode;
+
+			count++
+		}
+	}
+}
+
+template <class Type>
+void orderedLinkedList<Type>::insertFirst(const Type& newItem)
+{
+	insert(newItem);
+}
+
+template <class Type>
+void orderedLinkedList<Type>::insertLast(const Type& newItem)
+{
+	insert(newItem);
+}
+
+template <class Type>
+void orderedLinkedList<Type>::deleteNode(const Type& deleteItem)
+{
+	nodeType<Type> *current;
+	nodeType<Type> *trailCurrent;
+	bool found;
+
+	if (first = NULL)
+		cout << "Cannot delete from an empty list" << endl;
+	else
+	{
+		current = first;
+		found = false;
+
+		while(current != NULL && !found){
+			if (current->info >= deleteItem)
+				found = true;
+			else
+			{
+				trailCurrent = current;
+				current = current->link;
+			}
+		}
+
+		if (current == NULL)
+			cout << "The item to be delete is not in the list" << endl;
+		else
+		{
+			if (current->info == deleteItem){
+				if (first == current)
+				{
+					first = first->link;
+
+					if (first == NULL)
+						last = NULL;
+
+					delete current;
+					//count--;
+				}
+				else{
+					trailCurrent-link = current->link;
+
+					if (current == last)
+						last = trailCurrent;
+
+					delete current;
+				}
+
+			}
+			else
+				cout << "The item to be delted is not in the list." << endl;
+		}
+	}
 }
